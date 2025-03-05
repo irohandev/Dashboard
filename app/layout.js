2ddd2c25c6c76b4549d4469a29a64a79fe680d1d
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2025-03-05 21:39:57 +0530

    Project started

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,8 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Lama Dev Next.js Admin Dashboard',
-  description: 'Next.js Tutorial',
+  title: 'Admin Dashboard',
+  description: 'Admin Dashboard made using NextJS',
 }
 
 export default function RootLayout({ children }) {
