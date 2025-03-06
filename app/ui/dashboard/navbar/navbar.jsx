11f3ff25a0db6cbb2d86b670776dc964ef29e27f
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2025-03-06 22:08:54 +0530

    Navbar Update

--- app/ui/dashboard/navbar/navbar.jsx
+++ app/ui/dashboard/navbar/navbar.jsx
@@ -1,7 +1,23 @@
+"use client"
+import { usePathname } from "next/navigation";
+import styles from './navbar.module.css'
+import {
+  MdNotifications,
+  MdOutlineChat,
+  MdPublic,
+  MdSearch,
+} from "react-icons/md";
+
+
 const Navbar = () => {
-    return (
-      <div>Navbar</div>
-    )
-  }
+
+  const pathname = usePathname();
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>{pathname.split("/").pop()}</div>
+    </div>
+  )
+}
   
 export default Navbar
