e427c3fa269d9aa00fe4826cce8ba328e3ee4b29
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2025-03-07 20:10:50 +0530

    Navbar Updated

--- app/ui/dashboard/navbar/navbar.jsx
+++ app/ui/dashboard/navbar/navbar.jsx
@@ -18,8 +18,13 @@ const Navbar = () => {
       <div className={styles.title}>{pathname.split("/").pop()}</div>
       <div className={styles.menu}>
         <div className={styles.search}>
-        
-
+          <MdSearch/>
+          <input type="text" placeholder="Search..." className={styles.input}/>
+        </div>
+        <div className={styles.icon}>
+          <MdOutlineChat size={20}/>
+          <MdNotifications size={20}/>
+          <MdPublic size={20}/>
         </div>
 
       </div>
