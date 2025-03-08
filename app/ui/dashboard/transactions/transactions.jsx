7745d309d8eb955d1eea4fa2fa627b61ca4e66dc
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2025-03-08 21:38:59 +0530

    More componenets initialized

---
+++ app/ui/dashboard/transactions/transactions.jsx
@@ -0,0 +1,10 @@
+import styles from './transactions.module.css'
+
+
+const Transactions = () => {
+  return (
+    <div className={styles.container}>Transactions</div>
+  );
+};
+
+export default Transactions;
